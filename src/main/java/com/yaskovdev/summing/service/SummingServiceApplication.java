a56e5a1c03a8b9683ea143c029c20426af0d5cef
yaskovdev <yaskovdev@gmail.com>
2021-01-28 20:40:07 +0200

    Create empty Spring Boot project.

---
+++ src/main/java/com/yaskovdev/summing/service/SummingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yaskovdev.summing.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaskovdev/summing/service/SummingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaskovdev.summing.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
